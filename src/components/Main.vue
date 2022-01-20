faf75efe8045ab560e092ae7cd95dbc094a29965
Валерий <rasvv@mail.ru>
2022-01-20 22:26:10 +0300

    добавил РБ Активности

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,13 +3,14 @@
     <v-row class="calc__form bordered">
       <v-col cols="4" class="calc__selectnuclids borred">
         <v-row class="calc__types bordered mb-4">
-          <Radios :codRAO="codRAO" :idx="0" :row="true" />
+          <!-- Агрегатное состояние -->
+          <Radios :codRAO="codRAO" :idx="0" :row="true" />  
         </v-row>
 
         <v-row width="100%">
-          <v-col cols="5">
-            <div>
-              <v-radio-group v-model="favoriteNuclids" row class="calc__items">
+          <div class="nuclids__top">
+            <v-col cols="5" class="d-flex flex-column justify-stretch" height="100%">
+              <v-radio-group v-model="favoriteNuclids" row class="d-flex justify-start flex-column calc__items">
                 <v-radio label="Все" :value="0" class="calc__item"></v-radio>
                 <v-radio
                   label="Часто используемые"
@@ -18,95 +19,110 @@
                 ></v-radio>
               </v-radio-group>
               <v-text-field
+                class="d-flex justify-end flex-column"
                 label="Фильтр"
                 hide-details="auto"
                 clearable
                 v-model="filter"
               ></v-text-field>
-              <v-list max-height="50vh" dense>
-                <v-list-item-group class="left bordered">
+            </v-col>  
+            <v-col cols="1" class="d-flex flex-column">
+            </v-col>  
+            <v-col cols="6" class="nuclids__top-right">
+              <v-radio-group v-model="showUda" column class="calc__items">
+                <v-radio
+                  label="Удельная активность каждого нуклида"
+                  :value="0"
+                  class="calc__item"
+                ></v-radio>
+                <v-radio
+                  label="Общая удельная активность + %"
+                  :value="1"
+                  class="calc__item"
+                ></v-radio>
+                <v-radio
+                  label="Суммарная активность + масса"
+                  :value="2"
+                  class="calc__item"
+                ></v-radio>
+              </v-radio-group>
+              <v-text-field
+                v-show="showUda === 1"
+                label="Суммарная активность"
+                type="number"
+                hide-details="auto"
+                clearable
+                v-model="SumAct"
+              ></v-text-field>
+              <v-text-field
+                v-show="showUda === 2"
+                label="Удельная активность"
+                type="number"
+                hide-details="auto"
+                clearable
+                v-model="ObUdAct"
+              ></v-text-field>
+
+            </v-col>  
+          </div>
+          <div class="nuclids__bottom">
+            <v-col cols="5">
+              <div>
+                <v-list max-height="50vh" dense>
+                  <v-list-item-group class="left bordered">
+                    <v-list-item
+                      v-for="(item, i) in filteredNuclids"
+                      :key="i"
+                      @click="selected = filteredNuclids[i]"
+                      @dblclick="addNuclid"
+                    >
+                      <!-- {{ i + 1 }} -->
+                      {{ item.Name_RN }}
+                    </v-list-item>
+                  </v-list-item-group>
+                </v-list>
+              </div>
+            </v-col>
+
+            <v-col cols="1" class="buttons">
+              <v-btn @click="addNuclid">
+                <v-icon class="mx-0 mb-0">mdi-plus</v-icon>
+              </v-btn>
+              <v-btn @click="delNuclid">
+                <v-icon class="mx-0 mb-0">mdi-minus</v-icon>
+              </v-btn>
+            </v-col>
+
+            <v-col cols="6">
+              <v-list class="calc__nuclids">
+                <v-list-item-group class="right bordered">
                   <v-list-item
-                    v-for="(item, i) in filteredNuclids"
+                    v-for="(item, i) in selectedNuclids"
                     :key="i"
-                    @click="selected = filteredNuclids[i]"
-                    @dblclick="addNuclid"
+                    @click="selectedMin = selectedNuclids[i]"
+                    @dblclick="delNuclid"
                   >
-                    <!-- {{ i + 1 }} -->
-                    {{ item.Name_RN }}
+                    <div class="calc__nuclids-card">
+                      {{ item.Name_RN }}
+                      <v-text-field
+                        label="Удельная активность (кБк/кг)"
+                        v-model="item.UdA"
+                        type="number"
+                        @change="
+                          isdisb();
+                          calcCodRAO();
+                        "
+                        :rules="rules"
+                        hide-details="auto"
+                      ></v-text-field>
+                    </div>
+
+                    <!-- {{ item.Name_RN }} -->
                   </v-list-item>
                 </v-list-item-group>
               </v-list>
-            </div>
-          </v-col>
-
-          <v-col cols="1" class="buttons">
-            <v-btn @click="addNuclid">
-              <v-icon class="mx-0 mb-0">mdi-plus</v-icon>
-            </v-btn>
-            <v-btn @click="delNuclid">
-              <v-icon class="mx-0 mb-0">mdi-minus</v-icon>
-            </v-btn>
-          </v-col>
-
-          <v-col cols="6">
-            <!-- <v-btn 
-              :disabled="!enabledBTN"
-              @click="showKod = true"
-            >
-              Расчитать
-            </v-btn>             -->
-            <v-radio-group v-model="favoriteNuclids" column class="calc__items">
-              <v-radio
-                label="Удельная активность"
-                :value="0"
-                class="calc__item"
-              ></v-radio>
-              <v-radio
-                label="Суммарная активность + %"
-                :value="1"
-                class="calc__item"
-              ></v-radio>
-              <v-radio
-                label="Активность + масса"
-                :value="2"
-                class="calc__item"
-              ></v-radio>
-            </v-radio-group>
-            <v-text-field
-              label="Фильтр"
-              hide-details="auto"
-              clearable
-              v-model="filter"
-            ></v-text-field>
-
-            <v-list class="calc__nuclids">
-              <v-list-item-group class="right bordered">
-                <v-list-item
-                  v-for="(item, i) in selectedNuclids"
-                  :key="i"
-                  @click="selectedMin = selectedNuclids[i]"
-                  @dblclick="delNuclid"
-                >
-                  <div class="calc__nuclids-card">
-                    {{ item.Name_RN }}
-                    <v-text-field
-                      label="Удельная активность (кБк/кг)"
-                      v-model="item.UdA"
-                      type="number"
-                      @change="
-                        isdisb();
-                        calcCodRAO();
-                      "
-                      :rules="rules"
-                      hide-details="auto"
-                    ></v-text-field>
-                  </div>
-
-                  <!-- {{ item.Name_RN }} -->
-                </v-list-item>
-              </v-list-item-group>
-            </v-list>
-          </v-col>
+            </v-col>
+          </div>
         </v-row>
 
         <v-row class="bordered">
@@ -606,70 +622,86 @@ export default {
 <style lang="sass" scoped>
 // link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.css"
 html
-	box-sizing: border-box
-	margin: 0
-	padding: 0
+  box-sizing: border-box
+  margin: 0
+  padding: 0
 
 .calc
-	&__form
-		min-height: 1020px
-		height: 98vh
+  &__form
+    min-height: 1020px
+    height: 98vh
 
-	&__types
-		padding: 5px
+  &__types
+    padding: 5px
 
-	&__item
-		display: inline-block
+  &__item
+    display: inline-block
 
-	&__items
-		width: 100%
-		display: flex
-		justify-content: space-around
-		display: block
-		gap: 30px
+  &__items
+    width: 100%
+    display: flex
+    justify-content: space-around
+    display: block
+    gap: 30px
 
-	&__nuclids
-		width: 100%
+  &__nuclids
+    width: 100%
 
-		&-card
-			border-bottom: 1px solid #ccc
-			width: 100%
-			padding: 8px
-			justify-content: start
+    &-card
+      border-bottom: 1px solid #ccc
+      width: 100%
+      padding: 8px
+      justify-content: start
 
-	&__selectnuclids
+  &__selectnuclids
 // max-height: 95vh
 
 .v-btn:not(.v-btn--round).v-size--default
-	width: 100%
-	min-width: 0
-	padding: 0
-	margin-bottom: 10px
+  width: 100%
+  min-width: 0
+  padding: 0
+  margin-bottom: 10px
 
 .bordered
-	margin: 0
-	width: 100%
-	border: 1px solid lightblue
+  margin: 0
+  width: 100%
+  border: 1px solid lightblue
 
 .borred
-	height: 97vh
+  height: 97vh
 
 .buttons
-	padding: 0
-	gap: 30px
-	width: 100%
-	display: flex
-	flex-direction: column
-	justify-content: center
-	align-items: center
+  padding: 0
+  gap: 30px
+  width: 100%
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
 
 .left
-	overflow-y: scroll
-	max-height: 50vh
+  overflow-y: scroll
+  max-height: 40vh
 
 .right
-	height: 40vh
+  height: 40vh
 
 .h100
-	height: 100%
+  height: 100%
+
+.nuclids__top
+  width: 100%
+  display: flex
+  justify-content: space-between
+
+  &-left
+    display: flex
+    flex-direction: column
+    justify-content: space-between
+
+.nuclids__bottom
+  width: 100%
+  display: flex
+  justify-content: space-between
+  
 </style>
