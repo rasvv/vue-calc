e21e4713a6b7c31db5ddccf775800f45ffd49bb7
rasvv@mail.ru <rasvv>
2022-02-01 17:20:22 +0300

    check Deploy

--- src/deploy.sh
+++ src/deploy.sh
@@ -7,19 +7,20 @@ set -e
 npm run build
 
 # переход в каталог сборки
-cd dist
+# cd dist
 
 # если вы публикуете на пользовательский домен
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
+# git init
+git add dist
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:rasvv.github.io/vue-calc.git master:gh-pages
+# git push -f git@github.com:rasvv.github.io/vue-calc.git master:gh-pages
+git subtree push --prefix dist origin master:gh-pages
 
 cd -
