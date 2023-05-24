64f5d048657c242da553c94ddcda6153bd0db179
Валерий <rasvv@mail.ru>
2023-05-24 13:58:43 +0300

    Fix deploy

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # остановить публикацию при ошибках
-#set -e
+set -e
 
 # сборка
 npm run build
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:rasvv.github.io/vue-calc.git master:gh-pages
+git push -f git@github.com:rasvv.github.com/vue-calc.git master:gh-pages
 # git push -f git@github.com:rasvv/vue-mySite.git master:gh-pages
 
 cd -
