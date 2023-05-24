24c9d2ddaf29cce0a410cbee6095d9eb1c80ddc7
Валерий <rasvv@mail.ru>
2023-05-24 10:59:09 +0300

    dddd

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,32 +1,26 @@
 #!/usr/bin/env sh
 
-npm run build
-cd dist
-git add dist
-git commit -m 'deploy'
-#git subtree push --prefix dist origin gh-pages
-git push -f git@github.com:rasvv.github.io/vue-calc.git master:gh-pages
 # остановить публикацию при ошибках
-# set -e
+#set -e
 
 # сборка
-# npm run build
+npm run build
 
 # переход в каталог сборки
-# cd dist
+cd dist
 
 # если вы публикуете на пользовательский домен
 # echo 'www.example.com' > CNAME
 
-# git init
-# git add dist
-# git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:rasvv.github.io/vue-calc.git master:gh-pages
-# git subtree push --prefix dist origin gh-pages
+git push -f git@github.com:rasvv.github.io/vue-calc.git master:gh-pages
+# git push -f git@github.com:rasvv/vue-mySite.git master:gh-pages
 
-# cd -
+cd -
