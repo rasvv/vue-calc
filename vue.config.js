1c2b9717803a32658d1a9c6b521174a7852fc3d9
Валерий <rasvv@mail.ru>
2023-05-24 13:41:49 +0300

    Edit vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
-  publicPath: '/vue-calc/'
+	transpileDependencies: [
+		'vuetify'
+	],
+	publicPath: process.env.NODE_ENV === 'production' ? '/vue-calc/' : '/'
+	// publicPath: '/vue-mySite/'
 }
